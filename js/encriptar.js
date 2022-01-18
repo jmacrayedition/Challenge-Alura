1de63975b105b511977bbf93b6002f6adf3da9ed
Jose Espin <jmacrayedition@gmail.com>
2022-01-18 01:37:47 -0500

    Inicio

---
+++ js/encriptar.js
@@ -0,0 +1,59 @@
+
+var botonEncriptar = document.querySelector("#encriptar-texto");
+botonEncriptar.addEventListener("click", function (event) {
+
+    event.preventDefault();
+    const texto = document.getElementById('texto_one').value;
+    document.getElementById("texto_one").value = '';
+    mtdEncriptar(texto);
+
+});
+
+
+var botonDesencriptar = document.querySelector("#desencriptar-texto");
+botonDesencriptar.addEventListener("click", function (event) {
+
+    event.preventDefault();
+    const texto = document.getElementById('texto_one').value;
+
+    mtdDesencriptar(texto);
+
+});
+var botonCopiar = document.querySelector("#copiar-texto");
+botonCopiar.addEventListener("click", function (event) {
+    event.preventDefault();
+    mtdCopiar();
+    document.getElementById("texto_two").value = '';
+
+});
+/*
+Llaves de encriptación
+e: enter
+i: imes
+a: ai
+o: ober
+u: ufat
+
+Solo minúsculas, sin acentos
+
+*/
+function mtdEncriptar(texto) {
+
+
+    const encriptado = texto.replace(/e/gi, 'enter').replace(/i/gi, 'imes').replace(/a/gi, 'ai').replace(/o/gi, 'ober').replace(/u/gi, 'ufat');
+    document.getElementById("texto_two").value = encriptado
+
+}
+function mtdDesencriptar(texto) {
+
+
+    const desencriptado = texto.replace(/enter/gi, 'e').replace(/imes/gi, 'i').replace(/ai/gi, 'a').replace(/ober/gi, 'o').replace(/ufat/gi, 'u');
+    document.getElementById("texto_two").value = desencriptado
+}
+
+function mtdCopiar() {
+
+    var texto = document.getElementById('texto_two');
+    texto.select();
+    document.execCommand('copy');
+}
